813642c3f111370c614ead468259a8f535265da9
51memorundum <51shintani@gmail.com>
2026-07-12 22:34:50 +0900

    Update manuals.js

--- js/manuals.js
+++ js/manuals.js
@@ -1,1 +1,16 @@
+// manuals.json を読み込む
 
+fetch("data/manuals.json")
+    .then(response => response.json())
+    .then(data => {
+
+        console.log("マニュアル一覧");
+
+        console.log(data);
+
+    })
+    .catch(error => {
+
+        console.error("JSON読み込み失敗", error);
+
+    });
